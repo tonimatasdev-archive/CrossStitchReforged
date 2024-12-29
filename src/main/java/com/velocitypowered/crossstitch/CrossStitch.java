0be645c6e519312746924e81dce168aabce9bc24
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-12-29 21:54:00 +0100

    Port to Forge 1.18.2

---
+++ src/main/java/com/velocitypowered/crossstitch/CrossStitch.java
@@ -0,0 +1,11 @@
+package com.velocitypowered.crossstitch;
+
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.LoggerFactory;
+
+@Mod("crossstitch")
+public class CrossStitch {
+    public CrossStitch() {
+        LoggerFactory.getLogger(this.getClass()).info("CrossStitch started.");
+    }
+}
