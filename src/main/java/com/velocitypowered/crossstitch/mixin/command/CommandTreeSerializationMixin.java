4dec9f3ce59f020e6ef78c98ec8a3977ac061d64
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-12-29 21:46:10 +0100

    Port Forge

--- src/main/java/com/velocitypowered/crossstitch/mixin/command/CommandTreeSerializationMixin.java
+++ src/main/java/com/velocitypowered/crossstitch/mixin/command/CommandTreeSerializationMixin.java
@@ -2,49 +2,47 @@ package com.velocitypowered.crossstitch.mixin.command;
 
 import com.mojang.brigadier.arguments.ArgumentType;
 import io.netty.buffer.Unpooled;
-import net.minecraft.command.argument.serialize.ArgumentSerializer;
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.RegistryKey;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.commands.synchronization.ArgumentTypes;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.game.ClientboundCommandsPacket;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.Optional;
-
-@Mixin(targets = "net.minecraft.network.packet.s2c.play.CommandTreeS2CPacket$ArgumentNode")
+@SuppressWarnings({"rawtypes", "unchecked", "AddedMixinMembersNamePattern"})
+@Mixin(ClientboundCommandsPacket.class)
 public class CommandTreeSerializationMixin {
-    @Shadow @Final private ArgumentSerializer.ArgumentTypeProperties<?> properties;
-    private static final int MOD_ARGUMENT_INDICATOR = -256;
+    @Unique
+    private static final ResourceLocation MOD_ARGUMENT_INDICATOR = new ResourceLocation("crossstitch:mod_argument");
 
-    @Inject(method = "write(Lnet/minecraft/network/PacketByteBuf;Lnet/minecraft/command/argument/serialize/ArgumentSerializer;Lnet/minecraft/command/argument/serialize/ArgumentSerializer$ArgumentTypeProperties;)V",
-            at = @At("HEAD"), cancellable = true)
-    private static <A extends ArgumentType<?>, T extends ArgumentSerializer.ArgumentTypeProperties<A>> void writeNode$wrapInVelocityModArgument(PacketByteBuf buf, ArgumentSerializer<A, T> serializer, ArgumentSerializer.ArgumentTypeProperties<A> properties, CallbackInfo ci) {
-        Optional<RegistryKey<ArgumentSerializer<?, ?>>> entry = Registries.COMMAND_ARGUMENT_TYPE.getKey(serializer);
-
-        if (entry.isEmpty()) {
+    @Redirect(method = "writeNode", at = @At(value = "INVOKE", target = "Lnet/minecraft/commands/synchronization/ArgumentTypes;serialize(Lnet/minecraft/network/FriendlyByteBuf;Lcom/mojang/brigadier/arguments/ArgumentType;)V"))
+    private static void writeNode$wrapInVelocityModArgument(FriendlyByteBuf buf, ArgumentType<?> type) {
+        ArgumentTypes.Entry entry = ArgumentTypes.BY_CLASS.get(type.getClass());
+        
+        if (entry == null) {
+            buf.writeResourceLocation(new ResourceLocation(""));
             return;
         }
-        RegistryKey<ArgumentSerializer<?, ?>> keyed = entry.get();
-
-        if (keyed.getValue().getNamespace().equals("minecraft") || keyed.getValue().getNamespace().equals("brigadier")) {
-           return;
+        
+        if (entry.name.getNamespace().equals("minecraft") || entry.name.getNamespace().equals("brigadier")) {
+            buf.writeResourceLocation(entry.name);
+            entry.serializer.serializeToNetwork(type, buf);
+            return;
         }
-        ci.cancel();
 
         // Not a standard Minecraft argument type - so we need to wrap it
-        serializeWrappedArgumentType(buf, serializer, properties);
+        serializeWrappedArgumentType(buf, type, entry);
     }
 
-    private static <A extends ArgumentType<?>, T extends ArgumentSerializer.ArgumentTypeProperties<A>> void serializeWrappedArgumentType(PacketByteBuf packetByteBuf, ArgumentSerializer<A, T> serializer, ArgumentSerializer.ArgumentTypeProperties<A> properties) {
-        packetByteBuf.writeVarInt(MOD_ARGUMENT_INDICATOR);
-        packetByteBuf.writeVarInt(Registries.COMMAND_ARGUMENT_TYPE.getRawId(serializer));
+    @Unique
+    private static void serializeWrappedArgumentType(FriendlyByteBuf packetByteBuf, ArgumentType argumentType, ArgumentTypes.Entry entry) {
+        packetByteBuf.writeResourceLocation(MOD_ARGUMENT_INDICATOR);
+        packetByteBuf.writeResourceLocation(entry.name);
 
-        PacketByteBuf extraData = new PacketByteBuf(Unpooled.buffer());
-        serializer.writePacket((T) properties, extraData);
+        FriendlyByteBuf extraData = new FriendlyByteBuf(Unpooled.buffer());
+        entry.serializer.serializeToNetwork(argumentType, extraData);
 
         packetByteBuf.writeVarInt(extraData.readableBytes());
         packetByteBuf.writeBytes(extraData);
